dc8734ae82890793c12f7043c077fb122be0159d
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-07-09 19:31:32 +0300

    add imGE

--- Analogy.LogViewer.WindowsEventLogs/IAnalogy/ImagesContainer.cs
+++ Analogy.LogViewer.WindowsEventLogs/IAnalogy/ImagesContainer.cs
@@ -7,11 +7,21 @@ namespace Analogy.LogViewer.WindowsEventLogs.IAnalogy
 {
     public class ImagesContainer : IAnalogyComponentImages
     {
-        public Image GetLargeImage(Guid analogyComponentId) => Resources.OperatingSystem_32x32;
+        public Image GetLargeImage(Guid analogyComponentId)
+        {
+            if (analogyComponentId == EventLogDataFactory.id)
+               return Resources.OperatingSystem_32x32;
+            return null;
+        }
 
-        public Image GetSmallImage(Guid analogyComponentId) => Resources.OperatingSystem_16x16;
+        public Image GetSmallImage(Guid analogyComponentId)
+        {
+            if (analogyComponentId == EventLogDataFactory.id)
+                return Resources.OperatingSystem_16x16;
+            return null;
+        }
 
-        public Image GetOnlineConnectedLargeImage(Guid analogyComponentId) => Resources.OperatingSystem_32x32;
+            public Image GetOnlineConnectedLargeImage(Guid analogyComponentId) => Resources.OperatingSystem_32x32;
 
         public Image GetOnlineConnectedSmallImage(Guid analogyComponentId) => Resources.OperatingSystem_16x16;
         public Image GetOnlineDisconnectedLargeImage(Guid analogyComponentId) => Resources.OperatingSystem_32x32;

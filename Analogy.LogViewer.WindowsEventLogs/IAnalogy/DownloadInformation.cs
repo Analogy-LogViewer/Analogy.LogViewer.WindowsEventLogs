31f238c8bae4632b4aeca3698a0b4db55bd14806
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-10-25 22:02:25 +0200

    add download information

---
+++ Analogy.LogViewer.WindowsEventLogs/IAnalogy/DownloadInformation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+using Analogy.LogViewer.Template;
+
+namespace Analogy.LogViewer.WindowsEventLogs.IAnalogy
+{
+    public class SerilogDownloadInformation : Analogy.LogViewer.Template.AnalogyDownloadInformation
+    {
+        protected override string RepositoryURL { get; set; } = "https://api.github.com/repos/Analogy-LogViewer/Analogy.LogViewer.Analogy.LogViewer.WindowsEventLogs";
+        public override TargetFrameworkAttribute CurrentFrameworkAttribute { get; set; } = (TargetFrameworkAttribute)Assembly.GetExecutingAssembly().GetCustomAttribute(typeof(TargetFrameworkAttribute));
+
+        public override Guid FactoryId { get; set; } = EventLogPrimaryFactory.id;
+        public override string Name { get; set; } = "Windows Event Logs Data Provider";
+
+        private string? _installedVersionNumber;
+        public override string InstalledVersionNumber
+        {
+            get
+            {
+                if (_installedVersionNumber != null)
+                {
+                    return _installedVersionNumber;
+                }
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+                _installedVersionNumber = fvi.FileVersion;
+                return _installedVersionNumber;
+            }
+        }
+    }
+}

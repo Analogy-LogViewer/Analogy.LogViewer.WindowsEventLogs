2bf700e21f418aea49e02f6fce356e3048713917
Lior Banai <liorbanai@gmail.com>
2022-10-22 16:03:06 +0300

    update nugets

--- Analogy.LogViewer.WindowsEventLogs/IAnalogy/EventLogsDataProviderSettings.cs
+++ Analogy.LogViewer.WindowsEventLogs/IAnalogy/EventLogsDataProviderSettings.cs
@@ -8,14 +8,19 @@ using Analogy.LogViewer.WindowsEventLogs.Managers;
 
 namespace Analogy.LogViewer.WindowsEventLogs.IAnalogy
 {
-    public class EventLogsDataProviderSettings : UserSettingsFactory
+    public class EventLogsDataProviderSettings : TemplateUserSettingsFactory
     {
 
         public override string Title { get; set; } = "Windows Event Logs settings";
-        public override UserControl DataProviderSettings { get; set; } = new EventLogsSettings();
+        public override UserControl DataProviderSettings { get; set; } 
 
         public override Guid FactoryId { get; set; } = EventLogPrimaryFactory.id;
         public override Guid Id { get; set; } = new Guid("61774F7C-4F62-4A61-AD24-FC8263DF518A");
+
+        public override void CreateUserControl(IAnalogyLogger logger)
+        {
+            DataProviderSettings = new EventLogsSettings();
+        }
 
         public override Task SaveSettingsAsync()
         {
